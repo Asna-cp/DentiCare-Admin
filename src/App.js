718ad0d5a52a63944d4c636b786c2e1d04bed0d5
asn710 <cpasnaasn@gmail.com>
2023-02-03 12:18:49 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { ColorModeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { Routes, Route } from "react-router-dom";
+import Topbar from "./scenes/global/Topbar";
+import Sidebar from "./scenes/global/Sidebar";
+import Dashboard from "./scenes/dashboard";
+import Team from "./scenes/team";
+// import Resort from "./scenes/resort";
+// import Booking from "./scenes/booking";
+// import Facilities from "./scenes/facilities";
+// import Coupon from "./scenes/coupon";
+// import RoomType from "./scenes/roomtype";
+// import Geography from "./scenes/geography";
+// import Calendar from "./scenes/calendar";
 
 function App() {
+  const [theme, colorMode] = useMode();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ColorModeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <div className="app">
+          <Sidebar />
+          <main className="content">
+            <Topbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+              <Route path="/user" element={<Team />} />
+              {/* <Route path="/resort" element={<Resort />} /> */}
+              {/* <Route path="/booking" element={<Booking />} /> */}
+              {/* <Route path="/facilities" element={<Facilities />} /> */}
+              {/* <Route path="/coupon" element={<Coupon />} /> */}
+              {/* <Route path="/roomtype" element={<RoomType />} /> */}
+              {/* <Route path="/geography" element={<Geography />} /> */}
+              {/* <Route path="/calendar" element={<Calendar />} /> */}
+            </Routes>
+          </main>
+        </div>
+      </ThemeProvider>
+    </ColorModeContext.Provider>
   );
 }
 
