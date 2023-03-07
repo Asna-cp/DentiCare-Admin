950685a480b892a2c9eaa6719ad74cbc7a000eb0
asn710 <cpasnaasn@gmail.com>
2023-03-07 17:39:26 +0530

    admin login

--- src/App.js
+++ src/App.js
@@ -11,27 +11,38 @@ import AddDoctors from "./scenes/addDoctors";
 import Appointments from "./scenes/appointment";
 import Treatments from "./scenes/addTreatments";
 import ViewTreatments from "./scenes/addTreatments/viewTreatments";
+import Login from "./scenes/login/login";
 
 function App() {
   const [theme, colorMode] = useMode();
+  const adminToken = localStorage.getItem("adminToken");
+
   return (
     <ColorModeContext.Provider value={colorMode}>
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
-          <Sidebar />
-          <main className="content">
-            <Topbar />
+          {!adminToken ? (
             <Routes>
-              <Route path="/" element={<Dashboard />} />
-              <Route path="/doctors" element={<Doctors />} />
-              <Route path="/patients" element={<Patients />} />
-              <Route path="/addDoctors" element={<AddDoctors />} />
-              <Route path="/appointments" element={<Appointments />} />
-              <Route path="/addTreatments" element={<Treatments />} />
-              <Route path="/viewTreatments" element={<ViewTreatments />} />            
+              <Route path="/" element={<Login />} />
             </Routes>
-          </main>
+          ) : (
+            <>
+              <Sidebar />
+              <main className="content">
+                <Topbar />
+                <Routes>
+                  <Route path="/" element={<Dashboard />} />
+                  <Route path="/doctors" element={<Doctors />} />
+                  <Route path="/patients" element={<Patients />} />
+                  <Route path="/addDoctors" element={<AddDoctors />} />
+                  <Route path="/appointments" element={<Appointments />} />
+                  <Route path="/addTreatments" element={<Treatments />} />
+                  <Route path="/viewTreatments" element={<ViewTreatments />} />
+                </Routes>
+              </main>
+            </>
+          )}
         </div>
       </ThemeProvider>
     </ColorModeContext.Provider>
